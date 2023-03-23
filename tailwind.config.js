fa3537ab869707043140b474b5eec37f37ee4b76
coline85 <bodartcoline@gmail.com>
2023-03-23 13:48:22 +0100

    Installation tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-  './pages/**/*.{html,js}',
-  './components/**/*.{html,js}', 
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
 ],
   theme: {
     extend: {},
