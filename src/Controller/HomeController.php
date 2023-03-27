e74269535dc4d248b82f07e0fb1e426bc69c8e86
coline85 <bodartcoline@gmail.com>
2023-03-27 12:02:47 +0200

    modif appFixtures

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,52 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CommentaireRepository $repository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'commentaires' => $repository->findBy([], ['createdAt' => 'DESC']),
         ]);
     }
+
+    {
+    #[Route('/commentaire/new', name: 'app_commentaire_new')]
+    #[IsGranted('ROLE_USER')]
+    public function new(Request $request, EntityManagerInterface $manager, ReviewRepository $repository)
+    {
+        $review = new Review();
+        $form = $this->createForm(ReviewType::class, $review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Configurer la date de création et le créateur du commentaire (l'utilisateur connecté)
+            $review->setCreatedAt(new \DateTimeImmutable());
+            $review->setUser($this->getUser());
+
+            // Méthode 1 pour ajouter en BDD
+            $manager->persist($review);
+            $manager->flush();
+
+            // Méthode 2
+            // $repository->save($review, true);
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('review/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/review/delete/{id}', name: 'app_review_delete')]
+    #[Security("is_granted('ROLE_USER') and user === review.getUser()")]
+    public function delete(Review $review, EntityManagerInterface $manager)
+    {
+        $manager->remove($review);
+        $manager->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
 }
+
+
+?>
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Commentaire;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\DBAL\Driver\IBMDB2\Exception\Factory;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
@@ -19,20 +20,21 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create('fr_FR');
+
         $user = new User();
         $user->setName('Coline');
         $user->setEmail('bodartcoline@gmail.com');
+        $user->setBirthAt(\DateTimeImmutable::createFromMutable($faker->dateTime()));
         $user->setPseudo('Cok');
+        $user->setBiographie('');
         $user->setAvatar('C:\Users\bodar\Downloads\AvatarMaker_00d86.png');
-        // $user->setBirthAt('1985-06-16');
         $user->setPassword($this->hasher->hashPassword($user, 'password'));
         $user->setRoles(['ROLE_ADMIN']);
         $manager->persist($user);
 
-        $faker = Factory::create('fr_FR');
-
         for ($i = 0; $i < 10; $i++) {
-            $review = new Review();
+            $review = new Commentaire();
             $review->setContent($faker->text());
             $review->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTime()));
             $review->setUser($user);
